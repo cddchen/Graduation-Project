7e173c2b95d84970ba02c36eb5abe5591a941389
陈东 <cd@chendongdeMBP.lan>
2021-04-12 15:16:11 +0800

    version 2.0.1 fix the insect algorithm and use dp to calculate the short path length

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <bits/stdc++.h>
+//#include "/Applications/Polyspace/R2020a/extern/include/engine.h"
 using namespace std;
 const int maxn = 1e3 + 5;
 const int update_end_cnt = 1;
+const int INF = 0x3f3f3f3f;
 //#define cerr cout
 //记录空间中的点
+
 struct point {
     int x, y, z;
     point() {}
@@ -18,10 +21,12 @@ struct point {
         return os << "(" << p.x << ", " << p.y << " ," << p.z << ")";
     }
 };
+double get_dist(point a, point b) {
+    return sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y) + (a.z - b.z) * (a.z - b.z));
+}
 void read(point &t) {
     scanf("%d%d%d", &t.x, &t.y, &t.z);
 }
-typedef pair<int, point> PII;
 int n;
 //立方体模型
 struct node {
@@ -32,6 +37,8 @@ point st, ed;
 bool vis[maxn][maxn][maxn];
 bool check_vis(point p) { 
     if (p.x < 0 || p.y < 0 || p.z < 0)
+        return true;
+    if (vis[p.x][p.y][p.z])
         return true;
     return false;
 }
@@ -51,6 +58,7 @@ point q[maxn];
 int front = 0, rear = -1;
 int pre[maxn], min_path_length = -1, cnt = 0;
 vector<int> ed_idxs;
+//SPFA
 bool search() {
     cerr << "正在寻找最短路径" << endl;
     memset(dist, 0x3f, sizeof dist);
@@ -62,7 +70,6 @@ bool search() {
         vis[now.x][now.y][now.z] = 0;
         if (min_path_length != -1 && dist[now.x][now.y][now.z] > min_path_length) continue;
         if (now == ed) {
-            cerr << "找到一条最短路径了～" << front - 1 << endl;
             if (min_path_length == -1 || (dist[now.x][now.y][now.z] <= min_path_length)) {
                 min_path_length = dist[now.x][now.y][now.z];
                 ed_idxs.push_back(front - 1);
@@ -72,7 +79,7 @@ bool search() {
         
         for (int i = 0; i < 6; ++i) {
             point nx(now.x + dir[i][0], now.y + dir[i][1], now.z + dir[i][2]);
-            if (check_vis(nx) || vis[nx.x][nx.y][nx.z]) continue;
+            if (check_vis(nx)) continue;
             if (!set_dist(nx, now)) continue;
             q[++rear] = nx;
             vis[nx.x][nx.y][nx.z] = 1;
@@ -89,6 +96,7 @@ bool getpath() {
         cerr << "没有找到路径！" << endl;
         return 0;
     }
+    cerr << "共找到" << ed_idxs.size() << "条最短路径" << endl;
     cerr << "开始记录路径所经过点" << endl;
     for (auto now : ed_idxs) {
         while (now != 0) {
@@ -107,17 +115,19 @@ bool getpath() {
 //判断线段与面相交，P+tQ的向量与第i个box相交判断
 bool check_segment_cross_obstacle(point P, point Q, int i) {
     int L = box[i].lb.x, R = box[i].ru.x;
-    double t1, t2;
+    double t1 = 0.0, t2 = 1.0;
     //cerr << "vector " << P << "->" << Q << "insect judge with box" << i << endl;
     if (Q.x == 0) {
         if (!(L <= P.x && P.x <= R))
             return false;
     }
     else {
-        t1 = 1.0 * (L - P.x) / Q.x;
-        t2 = 1.0 * (R - P.x) / Q.x;
-        if (Q.x < 0) swap(t1, t2);
-        if (t1 > t2) return false;
+        double tmp1 = 1.0 * (L - P.x) / Q.x;
+        double tmp2 = 1.0 * (R - P.x) / Q.x;
+        if (Q.x < 0) swap(tmp1, tmp2);
+        if (tmp1 > tmp2) return false;
+        t1 = max(t1, tmp1);
+        t2 = min(t2, tmp2);
     }
     
     L = box[i].lb.y, R = box[i].ru.y;
@@ -126,10 +136,12 @@ bool check_segment_cross_obstacle(point P, point Q, int i) {
             return false;
     }
     else {
-        t1 = 1.0 * (L - P.y) / Q.y;
-        t2 = 1.0 * (R - P.y) / Q.y;
-        if (Q.y < 0) swap(t1, t2);
-        if (t1 > t2) return false;
+        double tmp1 = 1.0 * (L - P.y) / Q.y;
+        double tmp2 = 1.0 * (R - P.y) / Q.y;
+        if (Q.y < 0) swap(tmp1, tmp2);
+        if (tmp1 > tmp2) return false;
+        t1 = max(t1, tmp1);
+        t2 = min(t2, tmp2);
     }
     
     L = box[i].lb.z, R = box[i].ru.z;
@@ -138,13 +150,16 @@ bool check_segment_cross_obstacle(point P, point Q, int i) {
             return false;
     }
     else {                                   
-        t1 = 1.0 * (L - P.z) / Q.z;
-        t2 = 1.0 * (R - P.z) / Q.z;
-        if (Q.z < 0) swap(t1, t2);
-        if (t1 > t2) return false;
+        double tmp1 = 1.0 * (L - P.z) / Q.z;
+        double tmp2 = 1.0 * (R - P.z) / Q.z;
+        if (Q.z < 0) swap(tmp1, tmp2);
+        if (tmp1 > tmp2) return false;
+        t1 = max(t1, tmp1);
+        t2 = min(t2, tmp2);
     }
-    
-    return true;
+    if (t1 <= t2)
+        return true;
+    return false;
 }
 int check_segement_cross_all_obstacles(point s, point t) {
     for (int i = 0; i < n; ++i) {
@@ -154,18 +169,24 @@ int check_segement_cross_all_obstacles(point s, point t) {
     return -1;
 }
 //拟合路径 
+vector<double> dp_dist;
 vector<point> ans;
 void calc_path() {
     if (!getpath()) {
         cerr << "未找到路径！" << endl;
         return;
     }
+    dp_dist.resize(paths.size());
     for (int i = 0; i < paths.size(); ++i) {
+        if (!i) dp_dist[i] = 0;
+        else dp_dist[i] = 1.0 * INF;
         for (int j = 0; j < i; ++j) {
             if (check_segement_cross_all_obstacles(paths[j], paths[i]) == -1) {
-                pre[i] = j;
-                cerr << "link: " << paths[j] << ' ' << paths[i] << endl;
-                break;
+                double dist_of_ij = get_dist(paths[j], paths[i]);
+                if (dist_of_ij + dp_dist[j] < dp_dist[i]) {
+                    pre[i] = j;
+                    dp_dist[i] = dp_dist[j] + dist_of_ij;
+                }
             }
         }
     }
@@ -175,10 +196,13 @@ void calc_path() {
     ans.push_back(st);
     cerr << "拟合完毕，路线为：";
     reverse(ans.begin(), ans.end());
-    for (auto i : ans) {
-        cerr << i << "->";
+    for (int i = 0; i < ans.size(); ++i) {
+        if (!i) cerr << ans[i];
+        else cerr << "->" << ans[i];
     }
     cerr << endl;
+    cerr << "起点到终点的最短路径长度为：" << dp_dist[paths.size() - 1] << endl;
+    cerr << "起点到终点的欧式长度为：" << get_dist(st, ed) << endl;
 }
 int main()
 {
